8427b287c81520b7788e91ce2a2d5970eefbfd77
Iman Rajabi <iman.rjb@gmail.com>
2021-08-10 14:08:19 +0430

    API change

--- src/Http/Controllers/MicroserviceController.php
+++ src/Http/Controllers/MicroserviceController.php
@@ -13,7 +13,7 @@ class MicroserviceController
         $all_models = [];
         foreach (getAppModels() as $model) {
             $model = '\\App\\Models\\' . $model;
-            $all_models[strtolower(str_replace('\\App\Models\\', '', $model))] = $model::get()->count();
+            $all_models[strtolower(str_replace('\\App\Models\\', '', $model))] = $model::withTrashed()->get()->count();
             $deleted_models[strtolower(str_replace('\\App\Models\\', '', $model))] = $model::onlyTrashed()->get()->count();
             $updated_models[strtolower(str_replace('\\App\Models\\', '', $model))] = $model::latest()->first() ? $model::latest()->first()->updated_at->format('Y-m-d H:i:s') : null;
         }
