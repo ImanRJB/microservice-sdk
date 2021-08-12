f524e145f245f490780f6d0d4f5a366dfffae48d
Iman Rajabi <iman.rjb@gmail.com>
2021-08-13 00:48:17 +0430

    execution time middleware

--- src/Middleware/MeasureExecutionTime.php
+++ src/Middleware/MeasureExecutionTime.php
@@ -8,9 +8,10 @@ class MeasureExecutionTime
 {
     public function handle($request, Closure $next)
     {
+        $star_time = microtime(true);
         $response = $next($request);
 
-        $response->headers->set('X-Elapsed-Time', number_format((microtime(true) - LUMEN_START) * 1000, 2, '.', ''));
+        $response->headers->set('X-Elapsed-Time', number_format((microtime(true) - $star_time) * 1000, 2, '.', ''));
 
         return $response;
     }
