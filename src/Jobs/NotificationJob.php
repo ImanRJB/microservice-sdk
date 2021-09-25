eaee7d24af81496f730a7dcfb8a9693286f5e258
Iman Rajabi <iman.rjb@gmail.com>
2021-09-25 22:56:25 +0330

    some changes

--- src/Jobs/NotificationJob.php
+++ src/Jobs/NotificationJob.php
@@ -23,23 +23,23 @@ class NotificationJob extends Job
         $model_name = class_basename($model);
         if (file_exists(app_path() . '/Services/' . $model_name . 'NotificationCenter/' . $model_name . 'NotificationCenter.php')) {
             $notification_center = 'App\\Services\\' . $model_name . 'NotificationCenter\\' . $model_name . 'NotificationCenter';
-        }
-        switch ($this->method) {
-            case 'store':
-                $notification_center::createdNotification($model, $model->getChanges());
-                break;
-            case 'update':
-                $notification_center::updatedNotification($model, $model->getChanges());
-                break;
-            case 'delete':
-                $notification_center::deletedNotification($model, $model->getChanges());
-                break;
-            case 'restore':
-                $notification_center::restoredNotification($model, $model->getChanges());
-                break;
-            case 'forceDelete':
-                $notification_center::forceDeletedNotification($model, $model->getChanges());
-                break;
+            switch ($this->method) {
+                case 'store':
+                    $notification_center::createdNotification($model, $model->getChanges());
+                    break;
+                case 'update':
+                    $notification_center::updatedNotification($model, $model->getChanges());
+                    break;
+                case 'delete':
+                    $notification_center::deletedNotification($model, $model->getChanges());
+                    break;
+                case 'restore':
+                    $notification_center::restoredNotification($model, $model->getChanges());
+                    break;
+                case 'forceDelete':
+                    $notification_center::forceDeletedNotification($model, $model->getChanges());
+                    break;
+            }
         }
     }
 
